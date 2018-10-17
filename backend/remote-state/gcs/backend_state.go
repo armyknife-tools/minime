hashicorp/terraform

terraform: Ugly huge change to weave in new State and Plan types
Due to how often the state and plan types are referenced throughout
Terraform, there isn't a great way to switch them out gradually. As a
consequence, this huge commit gets us from the old world to a _compilable_
new world, but still has a large number of known test failures due to
key functionality being stubbed out.

The stubs here are for anything that interacts with providers, since we
now need to do the follow-up work to similarly replace the old
terraform.ResourceProvider interface with its replacement in the new
"providers" package. That work, along with work to fix the remaining
failing tests, will follow in subsequent commits.

The aim here was to replace all references to terraform.State and its
downstream types with states.State, terraform.Plan with plans.Plan,
state.State with statemgr.State, and switch to the new implementations of
the state and plan file formats. However, due to the number of times those
types are used, this also ended up affecting numerous other parts of core
such as terraform.Hook, the backend.Backend interface, and most of the CLI
commands.

Just as with 5861dbf3fc49b19587a31816eb06f511ab861bb4 before, I apologize
in advance to the person who inevitably just found this huge commit while
spelunking through the commit history.

backend/remote-state/gcs/backend_state.go
@@ -7,11 +7,12 @@ import (
 	"strings"
 
 	"cloud.google.com/go/storage"
+	"google.golang.org/api/iterator"
+
 	"github.com/hashicorp/terraform/backend"
 	"github.com/hashicorp/terraform/state"
 	"github.com/hashicorp/terraform/state/remote"
-	"github.com/hashicorp/terraform/terraform"
-	"google.golang.org/api/iterator"
+	"github.com/hashicorp/terraform/states"
 )
 
 const (
@@ -19,9 +20,9 @@ const (
 	lockFileSuffix  = ".tflock"
 )
 
-// States returns a list of names for the states found on GCS. The default
+// Workspaces returns a list of names for the workspaces found on GCS. The default
 // state is always returned as the first element in the slice.
-func (b *gcsBackend) States() ([]string, error) {
+func (b *gcsBackend) Workspaces() ([]string, error) {
 	states := []string{backend.DefaultStateName}
 
 	bucket := b.storageClient.Bucket(b.bucketName)
@@ -53,8 +54,8 @@ func (b *gcsBackend) States() ([]string, error) {
 	return states, nil
 }
 
-// DeleteState deletes the named state. The "default" state cannot be deleted.
-func (b *gcsBackend) DeleteState(name string) error {
+// DeleteWorkspace deletes the named workspaces. The "default" state cannot be deleted.
+func (b *gcsBackend) DeleteWorkspace(name string) error {
 	if name == backend.DefaultStateName {
 		return fmt.Errorf("cowardly refusing to delete the %q state", name)
 	}
@@ -85,7 +86,7 @@ func (b *gcsBackend) client(name string) (*remoteClient, error) {
 
 // State reads and returns the named state from GCS. If the named state does
 // not yet exist, a new state file is created.
-func (b *gcsBackend) State(name string) (state.State, error) {
+func (b *gcsBackend) StateMgr(name string) (state.State, error) {
 	c, err := b.client(name)
 	if err != nil {
 		return nil, err
@@ -127,7 +128,7 @@ func (b *gcsBackend) State(name string) (state.State, error) {
 			return baseErr
 		}
 
-		if err := st.WriteState(terraform.NewState()); err != nil {
+		if err := st.WriteState(states.NewState()); err != nil {
 			return nil, unlock(err)
 		}
 		if err := st.PersistState(); err != nil {

armyknife-tools/minime

backend/remote-state/gcs/backend_state.go
@@ -7,11 +7,12 @@ import (
 	"strings"
 
 	"cloud.google.com/go/storage"
+	"google.golang.org/api/iterator"
+
 	"github.com/hashicorp/terraform/backend"
 	"github.com/hashicorp/terraform/state"
 	"github.com/hashicorp/terraform/state/remote"
-	"github.com/hashicorp/terraform/terraform"
-	"google.golang.org/api/iterator"
+	"github.com/hashicorp/terraform/states"
 )
 
 const (
@@ -19,9 +20,9 @@ const (
 	lockFileSuffix  = ".tflock"
 )
 
-// States returns a list of names for the states found on GCS. The default
+// Workspaces returns a list of names for the workspaces found on GCS. The default
 // state is always returned as the first element in the slice.
-func (b *Backend) States() ([]string, error) {
+func (b *Backend) Workspaces() ([]string, error) {
 	states := []string{backend.DefaultStateName}
 
 	bucket := b.storageClient.Bucket(b.bucketName)
@@ -53,8 +54,8 @@ func (b *Backend) States() ([]string, error) {
 	return states, nil
 }
 
-// DeleteState deletes the named state. The "default" state cannot be deleted.
-func (b *Backend) DeleteState(name string) error {
+// DeleteWorkspace deletes the named workspaces. The "default" state cannot be deleted.
+func (b *Backend) DeleteWorkspace(name string) error {
 	if name == backend.DefaultStateName {
 		return fmt.Errorf("cowardly refusing to delete the %q state", name)
 	}
@@ -83,9 +84,9 @@ func (b *Backend) client(name string) (*remoteClient, error) {
 	}, nil
 }
 
-// State reads and returns the named state from GCS. If the named state does
+// StateMgr reads and returns the named state from GCS. If the named state does
 // not yet exist, a new state file is created.
-func (b *Backend) State(name string) (state.State, error) {
+func (b *Backend) StateMgr(name string) (state.State, error) {
 	c, err := b.client(name)
 	if err != nil {
 		return nil, err
@@ -127,7 +128,7 @@ func (b *Backend) State(name string) (state.State, error) {
 			return baseErr
 		}
 
-		if err := st.WriteState(terraform.NewState()); err != nil {
+		if err := st.WriteState(states.NewState()); err != nil {
 			return nil, unlock(err)
 		}
 		if err := st.PersistState(); err != nil {