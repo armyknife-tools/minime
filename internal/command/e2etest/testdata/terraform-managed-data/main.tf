titoluyo/terraform

add terraform_data e2e test

internal/command/e2etest/testdata/terraform-managed-data/main.tf
@@ -1,0 +1,18 @@
+resource "terraform_data" "a" {
+}
+
+resource "terraform_data" "b" {
+  input = terraform_data.a.uuid
+}
+
+resource "terraform_data" "c" {
+  trigger = terraform_data.b
+}
+
+resource "terraform_data" "d" {
+  input = [ terraform_data.b, terraform_data.c ]
+}
+
+output "d" {
+  value = terraform_data.d
+}

armyknife-tools/minime

internal/command/e2etest/testdata/terraform-managed-data/main.tf
@@ -1,0 +1,18 @@
+resource "terraform_data" "a" {
+}
+
+resource "terraform_data" "b" {
+  input = terraform_data.a.id
+}
+
+resource "terraform_data" "c" {
+  trigger = terraform_data.b
+}
+
+resource "terraform_data" "d" {
+  input = [ terraform_data.b, terraform_data.c ]
+}
+
+output "d" {
+  value = terraform_data.d
+}